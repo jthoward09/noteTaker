c9bdee43c4c678a03701cba6c8848c07a14a08b1
James Howard <jthoward09@gmail.com>
2021-01-27 21:51:53 -0500

    Remove unused code

--- server.js
+++ server.js
@@ -35,19 +35,6 @@ app.get("/api/notes", function(req, res) {
 });
 
 
-// Displays a single character, or returns false
-app.get("/api/characters/:character", function(req, res) {
-  var chosen = req.params.character;
-
-  for (var i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
-    }
-  }
-
-  return res.json(false);
-});
-
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
